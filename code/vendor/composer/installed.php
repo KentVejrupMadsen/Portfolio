168a946d0abe440dd5a03bc2a9ecec49f9501ce7
Kent vejrup Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-01-19 18:45:55 +0100

    project made

---
+++ code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'designermadsen/application',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '85702b4d27a16321c2b27b14ab4b3fa46a82f3a2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'designermadsen/application' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '85702b4d27a16321c2b27b14ab4b3fa46a82f3a2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
